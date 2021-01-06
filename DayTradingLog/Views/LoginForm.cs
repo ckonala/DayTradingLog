d7a3ec6812c38de34fe2b475b7c98a82b0f91ac1
Chakradhar Konala <ckonala@wgu.edu>
2021-01-06 17:31:38 -0600

    remove test code

--- DayTradingLog/Views/LoginForm.cs
+++ DayTradingLog/Views/LoginForm.cs
@@ -22,25 +22,6 @@ namespace DayTradingLog
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-            Stocks stocks = new Options();
-            stocks.PurchasePrice = stocks.TotalPurchasePrice = .68M;
-            var hh = stocks.TotalPurchasePrice;
-            var h1h = stocks.PurchasePrice;
-            var ee3 = stocks.TradeType;
-
-            Stocks stocks1 = new Stocks();
-            stocks1.PurchasePrice = stocks1.TotalPurchasePrice = 56.00M;
-            var ee = stocks1.TotalPurchasePrice;
-            var ee1 = stocks1.PurchasePrice;
-            var ee2 = stocks1.TradeType;
-
-            Stocks stocks2 = new PremiumStocks();
-            stocks2.Quantity = 100;
-            stocks2.PurchasePrice = stocks2.TotalPurchasePrice = 1.00M;
-            var ss = stocks2.TotalPurchasePrice;
-            var ss1 = stocks2.PurchasePrice;
-            var ee4 = stocks2.TradeType;
-
             if (!(userNameTextBox.Text is null) && userNameTextBox.Text.Length > 0)
             {
                 if (!(passwordTextBox.Text is null) && passwordTextBox.Text.Length > 0)
