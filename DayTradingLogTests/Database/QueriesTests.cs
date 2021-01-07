a01cead48702d333ae76c24b6176917509ca8d49
Chakradhar Konala <ckonala@wgu.edu>
2021-01-06 21:55:43 -0600

    Added Unit tests

---
+++ DayTradingLogTests/Database/QueriesTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayTradingLog.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayTradingLog.Database.Tests
+{
+    [TestClass()]
+    public class Tests
+    {
+        [TestMethod()]
+        public void Checkstocks()
+        {
+            Stocks stock = new Stocks()
+            {
+                Quantity = 10,
+                TotalPurchasePrice = 10,
+                TotalSalePrice = 15
+            };
+
+            Assert.AreEqual(10, stock.TotalPurchasePrice);
+            Assert.AreEqual(15, stock.TotalSalePrice);
+            Assert.AreEqual(5, stock.TradePL);
+        }
+
+        [TestMethod()]
+        public void CheckOptions()
+        {
+            Stocks stock = new Options()
+            {
+                Quantity = 10,
+                TotalPurchasePrice = 10,
+                TotalSalePrice = 15
+            };
+
+            Assert.AreEqual(10000, stock.TotalPurchasePrice);
+            Assert.AreEqual(15000, stock.TotalSalePrice);
+            Assert.AreEqual(5000, stock.TradePL);
+        }
+
+        [TestMethod()]
+        public void CheckPremiumStocks()
+        {
+            Stocks stock = new PremiumStocks()
+            {
+                Quantity = 10,
+                TotalPurchasePrice = 10,
+                TotalSalePrice = 15
+            };
+
+            Assert.AreEqual(102.65M, stock.TotalPurchasePrice);
+            Assert.AreEqual(152.65M, stock.TotalSalePrice);
+            Assert.AreEqual(50.00M, stock.TradePL);
+        }
+
+        [TestMethod()]
+        public void CheckOptionsNegitiveValue()
+        {
+            Stocks stock = new Options()
+            {
+                Quantity = 10,
+                TotalPurchasePrice = 10,
+                TotalSalePrice = 5
+            };
+
+            Assert.AreEqual(10000, stock.TotalPurchasePrice);
+            Assert.AreEqual(5000, stock.TotalSalePrice);
+            Assert.AreEqual(-5000, stock.TradePL);
+        }
+
+        [TestMethod()]
+        public void CheckPremiumStocksNegitiveValue()
+        {
+            Stocks stock = new PremiumStocks()
+            {
+                Quantity = 10,
+                TotalPurchasePrice = 10,
+                TotalSalePrice = 5
+            };
+
+            Assert.AreEqual(102.65M, stock.TotalPurchasePrice);
+            Assert.AreEqual(52.65M, stock.TotalSalePrice);
+            Assert.AreEqual(-50.00M, stock.TradePL);
+        }
+    }
+}
